e4c8517a2b148195c6abd7bce4c27fd00f2477bf
VedizAntonava <antonovyss2006@gmail.com>
2025-03-29 15:20:51 +0300

    CarService

---
+++ Project/Accounting for maintenance services/Accounting for maintenance services/BusinessLayer/Interfaces/ICarService.cs
@@ -0,0 +1,12 @@
+﻿using Accounting_for_maintenance_services.DataLayer;
+namespace Accounting_for_maintenance_services.BusinessLayer.Interfaces;
+
+public interface ICarService
+{
+    Car AddCar(Car car);
+    void UpdateCar(int carId, Car updatedCar);
+    void DeleteCar(int carId);
+    Car GetCarById(int carId);
+    List<Car> GetCarsByUserId(int userId);
+    List<Car> GetAllCars();
+}
