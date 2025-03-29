91e8ad20dc41ac807cd24ce64facd826c4d9c8f6
VedizAntonava <antonovyss2006@gmail.com>
2025-03-29 15:48:26 +0300

    Reminder

--- Project/Accounting for maintenance services/Accounting for maintenance services/DataLayer/Reminder.cs
+++ Project/Accounting for maintenance services/Accounting for maintenance services/DataLayer/Reminder.cs
@@ -2,5 +2,22 @@
 
 public class Reminder
 {
-    
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int CarId { get; set; }
+    public string Type { get; set; }
+    public DateTime DueDate { get; set; }
+    public string Frequency { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public Reminder(int id, int userId, int carId, string type, DateTime dueDate, string frequency)
+    {
+        Id = id;
+        UserId = userId;
+        CarId = carId;
+        Type = type;
+        DueDate = dueDate;
+        Frequency = frequency;
+        CreatedAt = DateTime.Now;
+    }
 }
