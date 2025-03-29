49caf02c2842cc3d536c0a3b7ddab4b4e7543a0d
VedizAntonava <antonovyss2006@gmail.com>
2025-03-29 16:40:31 +0300

    Maintenance

--- Project/Accounting for maintenance services/Accounting for maintenance services/DataLayer/Maintenance.cs
+++ Project/Accounting for maintenance services/Accounting for maintenance services/DataLayer/Maintenance.cs
@@ -2,5 +2,26 @@
 
 public class Maintenance
 {
-    
+    public int Id { get; set; }
+    public int CarId { get; set; }
+    public DateTime Date { get; set; }
+    public int Mileage { get; set; }
+    public string Category { get; set; }
+    public string PartNumber { get; set; }
+    public decimal Cost { get; set; }
+    public string Note { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public Maintenance(int id, int carId, DateTime date, int mileage, string category, string partNumber, decimal cost, string note)
+    {
+        Id = id;
+        CarId = carId;
+        Date = date;
+        Mileage = mileage;
+        Category = category;
+        PartNumber = partNumber;
+        Cost = cost;
+        Note = note;
+        CreatedAt = DateTime.Now;
+    }
 }
