c0afb61bf43e2b6e12e3aa97b42df6d930442337
VedizAntonava <antonovyss2006@gmail.com>
2025-03-29 20:48:30 +0300

    ServiceStation

--- Project/Accounting for maintenance services/Accounting for maintenance services/DataLayer/ServiceStation.cs
+++ Project/Accounting for maintenance services/Accounting for maintenance services/DataLayer/ServiceStation.cs
@@ -2,5 +2,24 @@
 
 public class ServiceStation
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public decimal Rating { get; set; }
+    public string ContactInfo { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public ServiceStation(int id, string name, string address, decimal latitude, decimal longitude, decimal rating, string contactInfo)
+    {
+        Id = id;
+        Name = name;
+        Address = address;
+        Latitude = latitude;
+        Longitude = longitude;
+        Rating = rating;
+        ContactInfo = contactInfo;
+        CreatedAt = DateTime.Now;
+    }
 }
