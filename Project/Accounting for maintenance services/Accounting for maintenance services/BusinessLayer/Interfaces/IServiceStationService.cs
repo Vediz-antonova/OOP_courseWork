ed8ab0e2e18f4f12f93a9639681c43120a563cc3
VedizAntonava <antonovyss2006@gmail.com>
2025-03-29 21:26:00 +0300

    ServiceStationService

---
+++ Project/Accounting for maintenance services/Accounting for maintenance services/BusinessLayer/Interfaces/IServiceStationService.cs
@@ -0,0 +1,13 @@
+﻿using Accounting_for_maintenance_services.DataLayer;
+namespace Accounting_for_maintenance_services.BusinessLayer.Interfaces;
+
+public interface IServiceStationService
+{
+    ServiceStation AddServiceStation(ServiceStation station);
+    void UpdateServiceStation(int id, ServiceStation updatedStation);
+    void DeleteServiceStation(int id);
+    ServiceStation GetServiceStationById(int id);
+    List<ServiceStation> GetAllServiceStations();
+    
+    List<ServiceStation> GetNearestServiceStations(decimal latitude, decimal longitude, decimal radius);
+}
