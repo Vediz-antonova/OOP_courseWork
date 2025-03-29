76f94496338523461afbb9fc9d9d4ebd5a15ab22
VedizAntonava <antonovyss2006@gmail.com>
2025-03-29 16:23:27 +0300

    ReminderService

---
+++ Project/Accounting for maintenance services/Accounting for maintenance services/BusinessLayer/Interfaces/IReminderService.cs
@@ -0,0 +1,13 @@
+﻿using Accounting_for_maintenance_services.DataLayer;
+namespace Accounting_for_maintenance_services.BusinessLayer.Interfaces;
+
+public interface IReminderService
+{
+    Reminder CreateReminder(Reminder reminder);
+    void UpdateReminder(int id, Reminder updatedReminder);
+    void DeleteReminder(int id);
+    Reminder GetReminderById(int id);
+    List<Reminder> GetRemindersByUserId(int userId);
+    List<Reminder> GetRemindersByCarId(int carId);
+    List<Reminder> GetAllReminders();
+}
